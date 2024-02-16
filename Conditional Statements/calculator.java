0b2413a14bf418e22e05cb737758b7a3fcf3db01
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-02-16 17:59:58 +0500

    Initial commit

---
+++ Conditional Statements/calculator.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Number:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter Second Number:");
+        int b = sc.nextInt();
+
+        System.out.println("Enter Operator (1: +, 2: -, 3: *, 4: /, 5: %) ");
+        int Operator = sc.nextInt();
+
+        switch (Operator) {
+            case 1:
+                System.out.println(a + b);
+                break;
+
+            case 2:
+                System.out.println(a - b);
+                break;
+
+            case 3:
+                System.out.println(a * b);
+                break;
+
+            case 4:
+                if (b == 0) {
+                    System.out.println("Invalid response");
+                } else {
+                    System.out.println(a / b);
+                }
+                break;
+
+            case 5:
+                if (b == 0) {
+                    System.out.println("Invalid response");
+                } else {
+                    System.out.println(a % b);
+                }
+                break;
+
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+        sc.close();
+
+    }
+}
